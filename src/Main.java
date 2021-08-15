9cd1b63962b284f2d77c904d4a6e8dc3ae1e208c
Victroria Anisimova <vitkakim@mail.ru>
2021-08-15 20:52:31 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 100;
-        int refill = 1010;
+        int refill = 1200;
+        int bonus;
 
         if (refill > 1_000) {
-            int bonus = refill / 100;
-            int total = balance + refill + bonus;
+            bonus = refill / 100;
             System.out.println("Кол-во бонусов = " + bonus);
-            System.out.println("Итоговая сумма = " + total);
         } else {
-            int total = balance + refill;
+            bonus = 0;
             System.out.println("Бонусов нет");
-            System.out.println("Итоговая сумма = " + total);
         }
+
+        int total = balance + refill + bonus;
+        System.out.println("Итоговая сумма = " + total);
     }
 }
